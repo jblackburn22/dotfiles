b9872834f8e8f65f6cfc23915aaa03448d006678
Jessica Blackburn <3924323+jblackburn22@users.noreply.github.com>
2021-10-01 14:51:51 -0400

    Add drawio

--- new_mac.d/python.sh
+++ new_mac.d/python.sh
@@ -1,5 +1,12 @@
+# Configure Pyenv
 append_to_file "$HOME/.zshrc" 'eval "$(pyenv init -)"'
 
 # CFLAGS=“-I$(brew --prefix openssl)/include -I$(brew --prefix bzip2)/include -I$(brew --prefix readline)/include -I$(xcrun --show-sdk-path)/usr/include” LDFLAGS=“-L$(brew --prefix openssl)/lib -L$(brew --prefix readline)/lib -L$(brew --prefix zlib)/lib -L$(brew --prefix bzip2)/lib” pyenv install --patch 3.6.8 < <(curl -sSL https://github.com/python/cpython/commit/8ea6353.patch\?full_index\=1)
 
 CFLAGS="-I$(brew --prefix openssl)/include -I$(brew --prefix bzip2)/include -I$(brew --prefix readline)/include -I$(xcrun --show-sdk-path)/usr/include" LDFLAGS="-L$(brew --prefix openssl)/lib -L$(brew --prefix readline)/lib -L$(brew --prefix zlib)/lib -L$(brew --prefix bzip2)/lib" pyenv install 3.8.6
+
+# Configure pipx
+if command -v pipx
+then
+	pipx ensurepath
+fi
